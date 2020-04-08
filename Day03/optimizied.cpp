7975ba695d94e232cad85b1e5e4574596d0298f2
glade46 <14358258+manasir66@users.noreply.github.com>
2020-04-08 23:49:29 +0800

    Changes

--- Day03/optimizied.cpp
+++ Day03/optimizied.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 class Solution{
   public:
-    
+    int maxSubarray(vector<int>& nums){
+        
+        int ans =INT_MIN, temp =0;
+
+        for(int val : nums){
+          temp += val;          //using kadanes algorithm
+          ans = max(ans, temp);
+          temp = max(temp, 0);
+        }
+        return ans;
+    }
   
 };
