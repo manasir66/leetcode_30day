e36b3da0416ad9318e2c998643b1564938a9a86a
glade46 <14358258+manasir66@users.noreply.github.com>
2020-04-09 11:52:43 +0800

    Day 08

---
+++ Day08/middle_of_linked_list.py
@@ -0,0 +1,20 @@
+
+ #Definition for singly-linked list.
+class ListNode:
+
+
+
+  def __init__(self, x):
+
+    self.val = x
+    self.next = None
+
+class Solution:
+
+  def middleNode(self, head: ListNode) -> ListNode:
+    
+    fast_pointer = head
+    slow_pointer = head
+
+    while ((fast_pointer != None) and (fast_pointer.next != None)):
+      pass
