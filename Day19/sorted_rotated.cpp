370a95a90b69c5717907d0c09baf53ca01054724
glade46 <14358258+manasir66@users.noreply.github.com>
2020-04-20 20:34:44 +0800

    Changes

--- Day19/sorted_rotated.cpp
+++ Day19/sorted_rotated.cpp
@@ -9,8 +9,13 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
 
-        int pv = find_pivot(0, nums.size()-1, nums);
-
+        int pivot_index = find_pivot(0, nums.size()-1, nums);
+        if(target > nums[pivot_index] && target < nums[nums.size()-1]){
+            binary_search(nums); //binary search on first part
+        }
+        else{ //binary search on the secon part
+            
+        }
         return 0;
     }
 
@@ -20,8 +25,23 @@ public:
         if (arr[mid_index] > arr[mid_index+1]){
             return (mid_index+1);
         }
+        else{
+            if (arr[st] > arr[mid_index]){
+                find_pivot(st, mid_index-1, arr);
+            }
+            else{
+                find_pivot(mid_index+1, ed, arr);
+            }
+        }
         return 0;
     }
+
+    int binary_search(vector<int>& arr){
+
+        return 0;
+    }
+
+
 };
 
 
