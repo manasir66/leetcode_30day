f2aff09c57e9bd288a44973dd825f0ba0c217d74
glade46 <14358258+manasir66@users.noreply.github.com>
2020-04-29 11:57:13 +0800

    Day 27

---
+++ Day27/maximal_square.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        
+        rows = len(matrix)
+        if rows == 0: 
+            return 0
+        cols = len(matrix[0])
+        height = 0
+        
+        
+        dp = [[0] * (cols +1) for _ in range(rows+1)]
+        
+        for i in range(1, rows+1):
+            for j in range(1, cols+1):
+                if matrix[i-1][j-1] == '1':
+                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+                    height = max(height, dp[i][j])
+                    
+        return height**2
+        
+        
